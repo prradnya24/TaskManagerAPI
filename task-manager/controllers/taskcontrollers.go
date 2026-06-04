6de8d1c4891d1d2259df40441dec01bef015b487
prradnya24 <pradnyalate27@gmail.com>
2026-06-04 21:08:34 +0530

    added get id api

--- task-manager/controllers/taskcontrollers.go
+++ task-manager/controllers/taskcontrollers.go
@@ -71,3 +71,17 @@ func DeleteTask(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": "Task deleted successfully"})
 }
+
+func GetTaskByID(c *gin.Context) {
+	id := c.Param("id")
+
+	var task models.Task
+
+	result := config.DB.First(&task, id)
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, task)
+}
