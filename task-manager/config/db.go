32a066922eda63b8d689b6f2ea477ca81f44ea75
prradnya24 <pradnyalate27@gmail.com>
2026-06-04 21:16:00 +0530

    deleted previous code

--- task-manager/config/db.go
+++ task-manager/config/db.go
@@ -1,29 +1,3 @@
-// package config
-
-// import (
-// 	"fmt"
-
-// 	"gorm.io/driver/mysql"
-// 	"gorm.io/gorm"
-// )
-
-// var DB *gorm.DB
-
-// func ConnectDB() {
-// 	dsn := "root:Root01@(127.0.0.1:3306)/task_manager?charset=utf8mb4&parseTime=True&loc=Local"
-
-// 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-// 	if err != nil {
-// 		panic("Failed to connect database")
-// 	}
-
-// 	fmt.Println("Database Connected")
-
-// 	DB = database
-
-// }
-
 
 package config
 
