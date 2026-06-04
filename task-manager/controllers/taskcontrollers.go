baade7067704342b9439b4adc7f9860905e39754
prradnya24 <pradnyalate27@gmail.com>
2026-06-04 20:57:40 +0530

    added update function

--- task-manager/controllers/taskcontrollers.go
+++ task-manager/controllers/taskcontrollers.go
@@ -28,3 +28,46 @@ func GetTasks(c *gin.Context) {
 
 	c.JSON(http.StatusOK, tasks)
 }
+
+
+func UpdateTask(c *gin.Context) {
+	id := c.Param("id")
+
+	var task models.Task
+
+	// check if task exists
+	result := config.DB.First(&task, id)
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+
+	// bind new data from request body
+	var updatedTask models.Task
+	if err := c.BindJSON(&updatedTask); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	// update only the fields sent
+	config.DB.Model(&task).Updates(updatedTask)
+
+	c.JSON(http.StatusOK, task)
+}
+
+func DeleteTask(c *gin.Context) {
+	id := c.Param("id")
+
+	var task models.Task
+
+	// check if task exists
+	result := config.DB.First(&task, id)
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+
+	config.DB.Delete(&task, id)
+
+	c.JSON(http.StatusOK, gin.H{"message": "Task deleted successfully"})
+}
