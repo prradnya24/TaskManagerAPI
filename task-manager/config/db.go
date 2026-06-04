f4a4a5b460c522e113527c696f117a4f3829da46
prradnya24 <pradnyalate27@gmail.com>
2026-06-04 21:15:00 +0530

    changes done in db file

--- task-manager/config/db.go
+++ task-manager/config/db.go
@@ -1,8 +1,37 @@
+// package config
+
+// import (
+// 	"fmt"
+
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
+
+// var DB *gorm.DB
+
+// func ConnectDB() {
+// 	dsn := "root:Root01@(127.0.0.1:3306)/task_manager?charset=utf8mb4&parseTime=True&loc=Local"
+
+// 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+// 	if err != nil {
+// 		panic("Failed to connect database")
+// 	}
+
+// 	fmt.Println("Database Connected")
+
+// 	DB = database
+
+// }
+
+
 package config
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,16 +39,29 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "root:Root01@(127.0.0.1:3306)/task_manager?charset=utf8mb4&parseTime=True&loc=Local"
+	// load .env file
+	err := godotenv.Load()
+	if err != nil {
+		panic("Failed to load .env file")
+	}
+
+	// read values from .env
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	// build DSN dynamically
+	dsn := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, password, host, port, name,
+	)
 
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
 	if err != nil {
-		panic("Failed to connect database")
+		panic("Failed to connect to database")
 	}
 
 	fmt.Println("Database Connected")
-
 	DB = database
-
 }
